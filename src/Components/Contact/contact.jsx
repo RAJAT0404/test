af7c730f72f3012605055ef42d5b3612e2ea8f79
rajat <sharma.rajat@rwit.io>
2023-11-08 16:54:39 +0530

    added env

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import SVG from "@/public/CONTACT.svg";
-// import ReCAPTCHA from "react-google-recaptcha";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function Contact() {
   const Router = useRouter();
@@ -194,10 +194,10 @@ export default function Contact() {
                 ></textarea>
               </div>
               <div className="form-group">
-                {/* <ReCAPTCHA
+                <ReCAPTCHA
                   onChange={setCaptcha}
                   sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-                /> */}
+                />
               </div>
 
               <button type="submit" className="btn btn-primary">
